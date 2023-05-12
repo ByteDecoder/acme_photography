064be852a5844745bea020c1d6b5985ac4f8c7f4
ByteDecoder <rreyes@canaryhealth.com>
2023-05-11 18:08:58 -0600

    README updated

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # acme_photography
+
 CSS3 playground
+
+## Instructions
+
+```bash
+yarn install
+yarn dev
+```
+
+## Production build
+
+```bash
+yarn build
+firebase login
+firebase deploy
+```
+
+## Firebase URL
+
+<https://acme-photography.firebaseapp.com/>
--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // Animate Smooth Scroll
 
-$(document).ready(function() {
-  $("#view-work").on("click", function() {
+$(document).ready(function () {
+  $("#view-work").on("click", function () {
     const images = $("#images").position().top;
     $("html, body").animate({ scrollTop: images }, 900);
   });
